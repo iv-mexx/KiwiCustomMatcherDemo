0f750209a8591fdfd6d97a700440d21e19d6f783
MeXx <mexx@devsub.net>
2014-02-09 17:40:17 +0100

    Initial Commit

---
+++ KiwiCusomtMatcherDemo/IVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IVAppDelegate.h
+//  KiwiCusomtMatcherDemo
+//
+//  Created by Markus Chmelar on 09.02.14.
+//  Copyright (c) 2014 Innovaptor OG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KiwiCusomtMatcherDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KiwiCusomtMatcherDemo
+//
+//  Created by Markus Chmelar on 09.02.14.
+//  Copyright (c) 2014 Innovaptor OG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IVAppDelegate class]));
+    }
+}
